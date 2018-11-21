8d62d06b154b70fb9a7c4d4694c6670596d17e3b
Robin Potze <robin-potze@live.nl>
2018-11-21 16:20:16 +0100

    Made First menu button Boiiii
    urmomgay

---
+++ Assets/Scripts/OnClickEffect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class OnClickEffect : MonoBehaviour {
+
+	public string SceneToNavTo;
+
+	public void Navigate ()
+	{
+		SceneManager.LoadScene(SceneToNavTo);
+	}
+}
